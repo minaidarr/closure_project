7a040d752c14484de5ca1ec520f8aadffff7f014
Maksat <minaidarr@gmail.com>
2020-10-27 07:53:34 +0500

    Initial Commit

---
+++ Closure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Closure
+//
+//  Created by Минайдар  Максат on 10/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
