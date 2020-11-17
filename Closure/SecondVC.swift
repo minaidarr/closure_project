0c754b58c347d58c148e933bdbd49d1eb1fce5c1
Maksat <minaidarr@gmail.com>
2020-11-17 08:04:40 +0500

    Second commit

---
+++ Closure/SecondVC.swift
@@ -0,0 +1,18 @@
+//
+//  SecondVC.swift
+//  Closure
+//
+//  Created by Минайдар  Максат on 11/17/20.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
